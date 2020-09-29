93b3da41f92f55d98bbfc45e47e6ef580edc76fd
Surajmal Kumar <surajkumar7252@gmail.com>
2020-09-29 22:07:23 +0530

    [Surajmal Kumar] Adding UC2 branch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,13 +6,21 @@ public class EmployeeWage {
 	              {
 
 	              System.out.println("Welcome to Employee Wage Computation Program");
-	              int IS_FULLTIME=1;                                     
-	          	System.out.println("Employee Attendance check");       
-	          	int a= (int)Math.floor(Math.random()*10)%2;            // computes the value of a.
-	          	if(a==IS_FULLTIME)                                      // checks if the value of a is equal to Employee.
-	          		System.out.println("Employee is present");
-	          	else
-	          		System.out.println("Employee is absent");
+	              int IS_FULL_TIME=1; 
+	              int EMP_PER_HOUR =20;                                             
+	      		
+	                int empHrs=0;                                                    //variables declared
+	                int empWage=0;  
+	          	
+	          	System.out.println("Daily Employee Wage Calculation System");
+                
+	    		double empCheck=Math.floor(Math.random()*10)%2;
+	    		if(empCheck== IS_FULL_TIME)                                       //COMPARISON
+	                       empHrs=8;
+	    		else
+	                       empHrs=0;
+	                   empWage=empHrs*EMP_PER_HOUR;                                 // Calculation
+	                   System.out.println("Emp Wage: "+empWage);
 	          	
 
 	              }
