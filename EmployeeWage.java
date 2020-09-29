e9e390b6fb9cd26300ac74afcd905ec35893aae9
Surajmal Kumar <surajkumar7252@gmail.com>
2020-09-29 22:14:00 +0530

    [Surajmal Kumar] Adding UC3 branch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,7 +6,8 @@ public class EmployeeWage {
 	              {
 
 	              System.out.println("Welcome to Employee Wage Computation Program");
-	              int IS_FULL_TIME=1; 
+	              int IS_FULL_TIME=2; 
+	              int IS_PART_TIME =1;
 	              int EMP_PER_HOUR =20;                                             
 	      		
 	                int empHrs=0;                                                    //variables declared
@@ -14,11 +15,13 @@ public class EmployeeWage {
 	          	
 	          	System.out.println("Daily Employee Wage Calculation System");
                 
-	    		double empCheck=Math.floor(Math.random()*10)%2;
+	    		double empCheck=Math.floor(Math.random()*10)%3;
 	    		if(empCheck== IS_FULL_TIME)                                       //COMPARISON
 	                       empHrs=8;
+	    		else if (empCheck== IS_PART_TIME)
+	                       empHrs=4;
 	    		else
-	                       empHrs=0;
+	    			   empHrs=0;
 	                   empWage=empHrs*EMP_PER_HOUR;                                 // Calculation
 	                   System.out.println("Emp Wage: "+empWage);
 	          	
