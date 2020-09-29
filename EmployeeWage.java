bf4300e94ddf5ddb08286483925b96837275d6cf
Surajmal Kumar <surajkumar7252@gmail.com>
2020-09-29 23:00:42 +0530

    [Surajmal Kumar] Adding UC8 branch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,22 +3,18 @@ public class EmployeeWage {
 	    
 	    public static final int IS_PART_TIME =1;             // CONSTANT      
 		public static final int IS_FULL_TIME =2;               
-		public static final int EMP_RATE_PER_HOUR =20; 
-		public static final int NUM_OF_WORKING_DAYS =20;
-		public static final int MAX_HRS_IN_MONTH =100;
 		
 		
 
-	              public static int computeEmpWage()
+	              public static int computeEmpWage(String company,int empRatePerHour,int numberOfWorkingDays,int maxHoursPerMonth)
 	              {
-	                System.out.println("Welcome to Employee Wage Computation Program");
-	              
+	               
 	                int empHrs=0,totalEmpHrs=0, totalWorkingDays=0;                                                    //variables declared
 	                
-	                while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)   // While Loop begins here.
+	                while(totalEmpHrs<=maxHoursPerMonth && totalWorkingDays<numberOfWorkingDays)   // While Loop begins here.
 	        		{
 	                	totalWorkingDays++; 
-	    		     int empCheck=(int)Math.floor(Math.random()*10)%2;
+	    		     int empCheck=(int)Math.floor(Math.random()*10)%3;
 	    		  
 	    		    switch(empCheck)
 	    		   {
@@ -36,12 +32,14 @@ public class EmployeeWage {
 	    		    totalEmpHrs+=empHrs;                                                           //total Employee Hours is obtained.
 	    			System.out.println("DAY#:"+totalWorkingDays+" Emp Hr: "+empHrs);              
 	    			}
-	                int totalEmpWage=totalEmpHrs+EMP_RATE_PER_HOUR;                                //total wage is being computed.
-	        		System.out.println("Total Emp Wage: "+ totalEmpWage); 
+	                int totalEmpWage=totalEmpHrs+empRatePerHour;                                //total wage is being computed.
+	        		System.out.println("Total Emp Wage for company: "+ company + " is: " + totalEmpWage); 
 	                return totalEmpWage;
 	             }
 	              public static void main(String[] args) {
-					computeEmpWage();
+					computeEmpWage("DMart",20,2,10);
+					computeEmpWage("Reliance",10,4,20);
+					
 				}
 }
 
