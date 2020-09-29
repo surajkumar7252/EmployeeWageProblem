e9a7011c2391879cfbd8927f4593769cd9d16d2d
Surajmal Kumar <surajkumar7252@gmail.com>
2020-09-29 22:00:34 +0530

    [Surajmal Kumar] Adding UC1 branch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,7 +5,15 @@ public class EmployeeWage {
 	              public static void main(String[] args) 
 	              {
 
-	              System.out.println("Welcome to Employee Wage Computation Program");  
+	              System.out.println("Welcome to Employee Wage Computation Program");
+	              int IS_FULLTIME=1;                                     
+	          	System.out.println("Employee Attendance check");       
+	          	int a= (int)Math.floor(Math.random()*10)%2;            // computes the value of a.
+	          	if(a==IS_FULLTIME)                                      // checks if the value of a is equal to Employee.
+	          		System.out.println("Employee is present");
+	          	else
+	          		System.out.println("Employee is absent");
+	          	
 
 	              }
 
