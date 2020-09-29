77030713c5e0c7bea53d096e4e58f0edf8dacd6e
Surajmal Kumar <surajkumar7252@gmail.com>
2020-09-29 22:22:21 +0530

    [Surajmal kumar] Adding UC4 branch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,28 +1,33 @@
 
 public class EmployeeWage {
-	
+	    
+	    public static final int IS_PART_TIME =1;             // CONSTANT      
+		public static final int IS_FULL_TIME =2;             // CONSTANT   
+		public static final int EMP_RATE_PER_HOUR =20; 
 
 	              public static void main(String[] args) 
 	              {
-
-	              System.out.println("Welcome to Employee Wage Computation Program");
-	              int IS_FULL_TIME=2; 
-	              int IS_PART_TIME =1;
-	              int EMP_PER_HOUR =20;                                             
-	      		
+	                System.out.println("Welcome to Employee Wage Computation Program");
+	              
 	                int empHrs=0;                                                    //variables declared
 	                int empWage=0;  
 	          	
-	          	System.out.println("Daily Employee Wage Calculation System");
-                
-	    		double empCheck=Math.floor(Math.random()*10)%3;
-	    		if(empCheck== IS_FULL_TIME)                                       //COMPARISON
-	                       empHrs=8;
-	    		else if (empCheck== IS_PART_TIME)
-	                       empHrs=4;
-	    		else
+	          	
+	    		int empCheck=(int)Math.floor(Math.random()*10)%2;
+	    		switch(empCheck)
+	    		{
+	    		case  IS_PART_TIME:
+	    		      empHrs=4;
+	    		      break;
+	    		case IS_FULL_TIME:
+	    		      empHrs=8;
+	    		      break;
+	    		default:
+	    			empHrs=0;
+	    		
+	    		}
 	    			   empHrs=0;
-	                   empWage=empHrs*EMP_PER_HOUR;                                 // Calculation
+	                   empWage=empHrs*EMP_RATE_PER_HOUR;                                 // Calculation
 	                   System.out.println("Emp Wage: "+empWage);
 	          	
 
